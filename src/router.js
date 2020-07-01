bc5d1909a84d9db07cb3b796ef7d1ea92bb13154
kevin <kevin@riou.pro>
2020-07-01 17:19:15 +0200

    Add team desc

--- src/router.js
+++ src/router.js
@@ -26,6 +26,14 @@ export default new Router({
           // this generates a separate chunk (about.[hash].js) for this route
           // which is lazy-loaded when the route is visited.
           component: () => import(/* webpackChunkName: "demo" */ './views/Screens.vue')
+        },
+        {
+          path: '/team',
+          name: 'team',
+          // route level code-splitting
+          // this generates a separate chunk (about.[hash].js) for this route
+          // which is lazy-loaded when the route is visited.
+          component: () => import(/* webpackChunkName: "demo" */ './views/Team.vue')
         }
       ]
     }
---
+++ src/views/Team.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="team">
+    <div class="row">
+      <div class="col team-member" v-for="(member,i) in team" :key="i">
+        <div class="img-container">
+          <img :src="member.photo" :alt="'Photo de '+member.name"/>
+        </div>
+        <div class="right-info">
+          <h3>{{ member.name }}</h3>
+          <h4>{{ member.role }}</h4>
+          <p>{{ member.description }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        team: [
+          {
+            name: 'Annaïs POIRIER',
+            photo: '/img/team/Annais.png',
+            role: 'Communication',
+            description: 'Petite description'
+          },
+          {
+            name: 'Pierre BLOUIN',
+            photo: '/img/team/Pierre.PNG',
+            role: 'Communication',
+            description: 'Petite description'
+          },
+          {
+            name: 'Estelle GRIMAUD',
+            photo: '/img/team/Estelle.PNG',
+            role: 'Communication',
+            description: 'Petite description'
+          },
+          {
+            name: 'Anton COATRIEUX',
+            photo: '/img/team/Anton.png',
+            role: 'Dev',
+            description: 'Petite description'
+          },
+          {
+            name: 'Kévin RIOU',
+            photo: '/img/team/Kevin.png',
+            role: 'Dev',
+            description: 'Petite description'
+          }
+        ]
+      }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+  .team{
+    .team-member{
+      width: 400px;
+      min-width: 400px;
+
+      display:flex;
+      flex-wrap: nowrap;
+      align-items: center;
+      justify-content: center;
+
+      .img-container{
+        background: linear-gradient(45deg, #32325d 0%, #F17851 100%);
+        border-radius: 50%;
+        height: 150px;
+        width: 150px;
+        margin: 50px 50px 0 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        img{
+          transform: scale(1.2);
+          margin: 0 0 50px 50px;
+          height: 150px;
+        }
+      }
+      
+
+      .right-info{
+        display: block;
+      }
+    }
+  }
+</style>
