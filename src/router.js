d352316a763a519e5f69543cca2bc090cb1e9f77
a.coatrieux <a.coatrieux@2isr.fr>
2020-07-20 20:05:40 +0200

    Head vue

--- src/router.js
+++ src/router.js
@@ -34,6 +34,13 @@ export default new Router({
           // this generates a separate chunk (about.[hash].js) for this route
           // which is lazy-loaded when the route is visited.
           component: () => import(/* webpackChunkName: "demo" */ './views/Team.vue')
+        },{
+          path: '/head',
+          name: 'head',
+          // route level code-splitting
+          // this generates a separate chunk (about.[hash].js) for this route
+          // which is lazy-loaded when the route is visited.
+          component: () => import(/* webpackChunkName: "demo" */ './views/Head.vue')
         }
       ]
     }
